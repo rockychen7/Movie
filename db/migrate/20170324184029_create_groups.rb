17f3127476467942f0c39cb79c0470bbac3ac4c3
陈帅 <rockychen7@gmail.com>
2017-03-25 03:02:05 +0800

    first line

--- db/migrate/20170324184029_create_groups.rb
+++ db/migrate/20170324184029_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :title
-      t.text :descirption
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324173115) do
+ActiveRecord::Schema.define(version: 20170324184029) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.text     "descirption"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
