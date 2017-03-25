8dc1cfee817b873598b734ad747f7c9dcf78d9ed
陈帅 <rockychen7@gmail.com>
2017-03-25 21:47:50 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @groups = Group.all
   end
@@ -15,6 +16,8 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new (group_params)
+    @group.user = current_user
+    
     if @group.save
     redirect_to groups_path
   else
